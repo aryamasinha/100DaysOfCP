1d9e762a2f1f9318efc17734ac30d85f5fdee213
aryama <aryamasinha864@gmail.com>
2020-12-04 22:40:55 +0530

    FlatLand (ICL1902)

---
+++ 100DaysOfCP/Day79/flatLand.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int count=0;
+	    while(n>=1){
+	        int sq = sqrt(n);
+	        n -= (sq*sq);
+	        count++;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
