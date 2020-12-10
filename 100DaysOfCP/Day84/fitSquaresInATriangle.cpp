81fa03aa17cee85a6b1efc6b0992ddd0ed064fc3
aryama <aryamasinha864@gmail.com>
2020-12-10 20:15:38 +0530

    Fit Squares in Triangle (TRISQ)

---
+++ 100DaysOfCP/Day84/fitSquaresInATriangle.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int ans=0;
+        while(n>2)
+        {
+            ans+=(n-2)/2;
+            n-=2;
+        }
+        cout<<ans<<'\n';
+    }
+   return 0; 
+}
